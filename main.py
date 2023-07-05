28c557f8d95b01a53d84decfb57ee72a951ba6ea
macbre <maciej.brencz@gmail.com>
2023-07-05 12:58:58 +0100

    Save the feed to the NDJSON file

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import logging
+import json
 from os import getenv
+from typing import TextIO
+
 from feed import get_facebook_feed
 
-def get_feed(feed_name: str, token: str):
+
+# http://ndjson.org/
+def save_feed_to_ndjson(feed_name: str, token: str, output: TextIO):
     posts = get_facebook_feed(feed_name, token)
 
     for post in posts:
         logging.info(f'{repr(post)}')
 
+        json.dump(post.dict(), sort_keys=True, fp=output)
+        output.write("\n")
 
 
 if __name__ == "__main__":
@@ -16,4 +23,5 @@ if __name__ == "__main__":
     token = getenv('FB_TOKEN', default='')
     logging.info(f'Using Facebook token: {token[0:3]}***{token[:3]}')
 
-    get_feed(feed_name='FarerskieKadry', token=token)
+    with open('farerskie_kadry.ndjson', 'wt') as fp:
+        save_feed_to_ndjson(feed_name='FarerskieKadry', token=token, output=fp)
