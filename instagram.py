ad4a64f5d51201e6b139b86a604e4da6b1215bb8
macbre <maciej.brencz@gmail.com>
2023-07-11 14:05:26 +0100

    instagram: get the instagram_account_id using the FB page name

--- instagram.py
+++ instagram.py
@@ -1,10 +1,24 @@
 import json
 import logging
-from typing import TextIO
+from typing import TextIO, Optional
 from os import getenv
 
-from feed import iterate_api_responses
+from feed import iterate_api_responses, make_request
 from dotenv import load_dotenv
+
+
+# # https://developers.facebook.com/docs/instagram-api/getting-started#before-you-start
+def ig_account_id_for_fb_page(fb_page: str, access_token: str) -> Optional[str]:
+    # https://developers.facebook.com/docs/instagram-api/reference/ig-user/
+    resp = make_request(f'/v17.0/{fb_page}', req_params={
+        'fields': ','.join(
+            ['connected_instagram_account{id,name,biography}', 'instagram_accounts{username,id,followed_by_count,media_count,profile_pic}', 'name', 'about']
+        ),
+        'access_token': access_token,
+    })
+    logging.info(f'Found IG account for {fb_page}: {repr(resp.get("instagram_accounts"))}, connected one: {repr(resp.get("connected_instagram_account"))}')
+
+    return resp.get('connected_instagram_account', {}).get('id')
 
 
 def save_feed_to_ndjson(ig_account: str, access_token: str, output: TextIO):
@@ -41,17 +55,7 @@ if __name__ == "__main__":
     token = getenv('FB_TOKEN', default='')
     logging.info(f'Using Facebook token: {token[0:3]}***{token[:3]}')
 
-    instagram_user_id = '1918062444917837'
-    instagram_account_id = '17841407952879412'
+    instagram_account_id = ig_account_id_for_fb_page(fb_page='FarerskieKadry', access_token=token)  # 17841407952879412
 
     with open('farerskie_kadry_ig.ndjson', 'wt') as fp:
         save_feed_to_ndjson(ig_account=instagram_account_id, access_token=token, output=fp)
-
-    # https://developers.facebook.com/docs/instagram-api/getting-started#before-you-start - you need a pro IG account
-    # an your FB access token needs to have the 'instagram_basic' right
-    # resp = http.get(f'https://graph.facebook.com/v17.0/{fb_page}', params={
-    #     'fields': ','.join(['connected_instagram_account', 'instagram_accounts{username,id,followed_by_count,media_count,profile_pic}', 'name', 'about']),
-    #     'access_token': token,
-    # })  # -> 186725698400085
-    # resp.raise_for_status()
-    # logging.info('API response: %r', resp.json())
