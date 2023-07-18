150073f08894ef979085e772d3fa31d1de25c337
macbre <maciej.brencz@gmail.com>
2023-07-18 11:14:36 +0100

    Save Facebook RSS to /docs

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import logging
 import json
+from itertools import islice
 from os import getenv
 from typing import TextIO
 
 from dotenv import load_dotenv
 
 from facebook import get_facebook_feed
+from rss import RssFeedWriter
+from utils import response_entity_to_rss_item
 
 
 # http://ndjson.org/
 def save_feed_to_ndjson(feed_name: str, access_token: str, output: TextIO):
-    posts = get_facebook_feed(feed_name, access_token)
+    for entry in get_facebook_feed(feed_name, access_token):
+        logging.info(f'{repr(entry)}')
 
-    for post in posts:
-        logging.info(f'{repr(post)}')
-
-        json.dump(post.dict(), sort_keys=True, fp=output)
+        json.dump(entry.dict(), sort_keys=True, fp=output)
         output.write("\n")
 
 
@@ -29,5 +30,27 @@ if __name__ == "__main__":
     token = getenv('FB_TOKEN', default='')
     logging.info(f'Using Facebook token: {token[0:3]}***{token[:3]}')
 
-    with open('farerskie_kadry.ndjson', 'wt') as fp:
-        save_feed_to_ndjson(feed_name='FarerskieKadry', access_token=token, output=fp)
+    # with open('farerskie_kadry.ndjson', 'wt') as fp:
+    #     save_feed_to_ndjson(feed_name='FarerskieKadry', access_token=token, output=fp)
+
+    # save to the RSS feed
+    with open('docs/facebook.xml', 'wt') as fp:
+        with RssFeedWriter(
+                out=fp,
+                title='Farerskie Kadry na Facebooku',
+                link='https://www.facebook.com/FarerskieKadry/',
+                description='Suma miliona drobnych, banalnych sytuacji, miejsc, '
+                            'ludzi uwiecznionych na cyfrowych kadrach i w nostalgicznych zakamarkach pamięci'
+        ) as feed:
+            ig_feed = islice(get_facebook_feed(feed_name='FarerskieKadry', token=token), 30)
+
+            for post in ig_feed:
+                # do not add posts that share links from the blog
+                if post.link and 'farerskiekadry.pl' in post.link:
+                    continue
+
+                feed.add_item(
+                    response_entity_to_rss_item(post)
+                )
+
+    logging.info('Done')
